735ecf2242b83aa8df369b7e02cc11aa33adb1c2
angelatto <pudri109@gmail.com>
2021-05-11 11:44:20 +0900

    [update] response status

--- public/js/controllers/authController.js
+++ public/js/controllers/authController.js
@@ -3,7 +3,13 @@ angular.module("app")
         
         $scope.login = (user) => {
             authService.login(user)
-            .then((response) => {
+            .then((response) => { // status 200 
+                /*
+                    응답 상태 코드가 200이면 정상 로그인 
+                    응답 상태 코드가 401이면 아이디 또는 비번 틀림 
+                */
+                console.log("응답 코드: ", response.status);
+
                 $rootScope.uid = response.data.uid;
                 $rootScope.authToken = response.data.authToken;
 
@@ -12,8 +18,11 @@ angular.module("app")
             
                 $location.url("/profit_m");
             })
-            .catch ((response) => {
-                $window.alert("로그인 실패:" + response.data.message);
+            .catch ((response) => {  // status 401 
+                console.log("응답 코드: ", response.status);
+               // console.log("에러 코드: ", response.data.result.id);
+                console.log("응답 데이터: ", response.data.result.message);
+                $window.alert("로그인 실패: " + response.data.result.message);
             });
         };
     });
